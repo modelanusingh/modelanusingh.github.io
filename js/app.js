d070e99bbabc9b1cc710568c7a2b830f8a68e138
kumarbhushan <kumarbhushan@live.in>
2017-01-31 17:36:17 +0530

    roting_fix

--- js/app.js
+++ js/app.js
@@ -1,35 +1,58 @@
-(function($) {
+(function ($) {
 
-  var app = $.sammy('#app', function() {
+  var app = $.sammy('#app', function () {
     this.use('Template');
-    this.get('#/', function(context) {
+    this.get('#/', function (context) {
       context.app.swap('');
-     
-        context.render('templates/home.html',{})
-               .appendTo(context.$element());
-
-                 $('.carousel').carousel({
-        interval: 5000 //changes the speed
-    })
-     
+      context.render('templates/home.html', {})
+        .appendTo(context.$element());
+      setTimeout(function () {
+        $('.carousel').carousel({
+          interval: 5000 //changes the speed
+        })
+      }, 5000);
     });
-    
-    this.get('#/about', function(context) {
-        var str=location.href.toLowerCase();
-        context.app.swap('');
-        context.render('templates/about.html', {})
-               .appendTo(context.$element());
-    });  
 
-    this.before('.*', function() {
-        var hash = document.location.hash;
-        $("nav").find("a").removeClass("current");
-        $("nav").find("a[href='"+hash+"']").addClass("current");
-   });
+    this.get('#/bio', function (context) {
+      var str = location.href.toLowerCase();
+      context.app.swap('');
+      context.render('templates/bio.html', {})
+        .appendTo(context.$element());
+    });
+     this.get('#/headshoot', function (context) {
+      var str = location.href.toLowerCase();
+      context.app.swap('');
+      context.render('templates/headshoot.html', {})
+        .appendTo(context.$element());
+    });
+     this.get('#/modeling', function (context) {
+      var str = location.href.toLowerCase();
+      context.app.swap('');
+      context.render('templates/modeling.html', {})
+        .appendTo(context.$element());
+    });
+     this.get('#/stats', function (context) {
+      var str = location.href.toLowerCase();
+      context.app.swap('');
+      context.render('templates/stats.html', {})
+        .appendTo(context.$element());
+    });
+     this.get('#/video', function (context) {
+      var str = location.href.toLowerCase();
+      context.app.swap('');
+      context.render('templates/video.html', {})
+        .appendTo(context.$element());
+    });
+
+    this.before('.*', function () {
+      var hash = document.location.hash;
+      $("nav").find("a").removeClass("current");
+      $("nav").find("a[href='" + hash + "']").addClass("current");
+    });
 
   });
 
-  $(function() {
+  $(function () {
     app.run('#/about');
   });
 
